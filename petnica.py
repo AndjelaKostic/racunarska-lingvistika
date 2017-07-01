1c49b33b9179800bc39f5ff0d1e11b5e71405928
AndjelaKostic <lelako99@gmail.com>
2017-07-01 18:04:07 +0200

    Add files via upload

---
+++ petnica.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  1 17:27:52 2017
+
+@author: Korisnik
+"""
+petnica={"A":["ANT","AHL","IST","DIZ","DRH","LIN","PSIH"],"B":["BIO","BMD","EBH","HEM","GEO"],"C":["AST","FIZ","MAT","PFE","RAČ","TEH"]}
+print(petnica["A"]) 
+print(petnica["B"])
+print(petnica["C"])
